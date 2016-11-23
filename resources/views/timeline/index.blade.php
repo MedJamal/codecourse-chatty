2836a782482a72baf88758f29633a513ae494ced
Ranie Santos <ranietots32@gmail.com>
2016-11-23 22:08:28 +0800

    [part 26 done] show statuses on timeline

--- resources/views/timeline/index.blade.php
+++ resources/views/timeline/index.blade.php
@@ -21,7 +21,62 @@
 
 	<div class="row">
 		<div class="col-lg-5">
-			
+			@forelse($statuses as $status)
+				<div class="media">
+					<a class="pull-left" href="{{ route('profile.index', ['username' => $status->user->username]) }}">
+						<img class="media-object" alt="{{ $status->user->getNameOrUsername() }}" src="{{ $status->user->getAvatarUrl() }}">
+					</a>
+					<div class="media-body">
+						<h4 class="media-heading">
+							<a href="{{ route('profile.index', ['username' => $status->user->username]) }}">{{ $status->user->getNameOrUsername() }}</a>
+						</h4>
+						<p>{{ $status->body }}</p>
+						<ul class="list-inline">
+							<li>{{ $status->created_at->diffForHumans() }}</li>
+							@if($status->user->id !== Auth::id())
+								<li><a href="#">Like</a></li>
+							@endif
+							{{-- <li>{{ $status->likes->count() }} {{ str_plural('like', $status->likes->count()) }}</li> --}}
+						</ul>
+						{{--
+						@foreach($status->replies as $reply)
+							<div class="media">
+								<a href="{{ route('profile.index', ['username' => $reply->user->username]) }}" class="pull-left">
+									<img src="{{ $reply->user->getAvatarUrl() }}" alt="{{ $reply->user->getNameOrUsername() }}" class="media-object">
+								</a>
+								<div class="media-body">
+									<h5 class="media-heading"><a href="{{ route('profile.index', ['username' => $reply->user->username]) }}">{{ $reply->user->getNameOrUsername() }}</a></h5>
+									<p>{{ $reply->body }}</p>
+									<ul class="list-inline">
+										<li>{{ $reply->created_at->diffForHumans() }}</li>
+										@if($reply->user->id !== Auth::user()->id)
+											<li><a href="{{ route('status.like', ['statusId' => $reply->id]) }}">Like</a></li>
+										@endif
+										<li>{{ $reply->likes->count() }} {{ str_plural('like', $reply->likes->count()) }}</li>
+									</ul>
+								</div>
+							</div>
+						@endforeach
+
+						{{ route('status.reply', ['statusId' => $status->id]) }}
+						--}}
+						<form action="#" role="form" method="post">
+							<div class="form-group {{ $errors->has("reply-{$status->id}") ? 'has-error' : '' }}">
+								<textarea name="reply-{{ $status->id }}" rows="2" class="form-control" placeholder="Reply to this status"></textarea>
+								@if($errors->has("reply-{$status->id}"))
+									<span class="help-block">{{ $errors->first("reply-{$status->id}") }}</span>
+								@endif
+							</div>
+							<input type="submit" value="Reply" class="btn btn-default btn-sm">
+							{!! csrf_field() !!}
+						</form>
+					</div>
+				</div>
+			@empty
+				<p>There's nothing in your timeline, yet.</p>
+			@endforelse
+
+			{!! $statuses->render() !!}
 		</div>
 	</div>
 @stop
