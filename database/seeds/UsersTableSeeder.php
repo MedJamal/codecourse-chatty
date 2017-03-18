2813d1aa8017f5f49a611169844bf8dce21a2678
Ranie Santos <ranietots32@gmail.com>
2017-03-19 02:22:36 +0800

    minor change to seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,44 +1,46 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Chatty\Models\User;
 
 class UsersTableSeeder extends Seeder
 {
 	public function run()
 	{
-		User::create([
-			'email'		=> 'ransan32@yahoo.com',
-			'username'	=> 'ransan32',
-			'password'	=> bcrypt('password'),
-			'firstname'	=> 'Ranie',
-			'lastname'	=> 'Santos',
-			'location'	=> 'Manila, PH',
-		]);
-		
-		User::create([
-			'email'		=> 'alex@codecourse.com',
-			'username'	=> 'alex',
-			'password'	=> bcrypt('password'),
-			'firstname'	=> 'Alex',
-			'lastname'	=> 'Garrett',
-			'location'	=> 'London, UK',
-		]);
-		
-		User::create([
-			'email'		=> 'dale@codecourse.com',
-			'username'	=> 'dale',
-			'password'	=> bcrypt('password'),
-			'firstname'	=> 'Dale',
-			'lastname'	=> 'Garrett',
-		]);
+		DB::table('users')->truncate();
 
-		User::create([
-			'email'		=> 'billy@codecourse.com',
-			'username'	=> 'billy',
-			'password'	=> bcrypt('password'),
-			'firstname'	=> 'Billy',
-			'lastname'	=> 'Garrett',
-		]);
-	}
-}
+		DB::table('users')->insert([
+			[
+				'email'		=> 'ransan32@yahoo.com',
+				'username'	=> 'ransan32',
+				'password'	=> bcrypt('password'),
+				'firstname'	=> 'Ranie',
+				'lastname'	=> 'Santos',
+				'location'	=> 'Manila, PH',
+			],
+			[
+				'email'		=> 'alex@codecourse.com',
+				'username'	=> 'alex',
+				'password'	=> bcrypt('password'),
+				'firstname'	=> 'Alex',
+				'lastname'	=> 'Garrett',
+				'location'	=> 'London, UK',
+			],
+			[
+				'email'		=> 'dale@codecourse.com',
+				'username'	=> 'dale',
+				'password'	=> bcrypt('password'),
+				'firstname'	=> 'Dale',
+				'lastname'	=> 'Garrett',
+				'location'	=> null,
+			],
+			[
+				'email'		=> 'billy@codecourse.com',
+				'username'	=> 'billy',
+				'password'	=> bcrypt('password'),
+				'firstname'	=> 'Billy',
+				'lastname'	=> 'Garrett',
+				'location'	=> null,
+			],
+		]); // insert
+	} // run
+} // class
