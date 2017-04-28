c5341ecd6a4d0de8ba76a37c1166e407b18cd510
Ranie Santos <ranietots32@gmail.com>
2017-04-28 09:35:18 +0800

    updated seeders

---
+++ database/seeds/StatusesTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Carbon\Carbon;
+use Chatty\Models\Status;
+use Illuminate\Database\Seeder;
+
+class StatusesTableSeeder extends Seeder
+{
+	public function run()
+	{
+		Status::truncate();
+
+		$faker = Faker\Factory::create();
+
+		foreach (range(1, 4) as $key) {
+			$timePosted = Carbon::now()->subDays(4 - $key);
+
+			Status::create([
+				'user_id' => $key,
+				'body' => $faker->sentence(5, false),
+				'created_at' => $timePosted,
+				'updated_at' => $timePosted,
+			]);
+		}
+	} // run
+}
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,62 +1,44 @@
 <?php
 
+use Chatty\Models\User;
 use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
 {
 	public function run()
 	{
-		DB::table('users')->truncate();
+		User::truncate();
 
-		DB::table('users')->insert([
-			[
-				'email'		=> 'ransan32@yahoo.com',
-				'username'	=> 'ransan32',
+		User::create([
+			'email'		=> 'ransan32@yahoo.com',
+			'username'	=> 'ransan32',
+			'password'	=> bcrypt('password'),
+			'firstname'	=> 'Ranie',
+			'lastname'	=> 'Santos',
+			'location'	=> 'Manila, PH',
+		]);
+
+		User::create([
+			'email'		=> 'alex@codecourse.com',
+			'username'	=> 'alex',
+			'password'	=> bcrypt('password'),
+			'firstname'	=> 'Alex',
+			'lastname'	=> 'Garrett',
+			'location'	=> 'London, UK',
+		]);
+
+		$faker = Faker\Factory::create();
+
+		$names = ['billy', 'cathy', 'dale', 'elma'];
+
+		foreach ($names as $name) {
+			User::create([
+				'email'		=> $name . '@codecourse.com',
+				'username'	=> $name,
 				'password'	=> bcrypt('password'),
-				'firstname'	=> 'Ranie',
-				'lastname'	=> 'Santos',
-				'location'	=> 'Manila, PH',
-			],
-			[
-				'email'		=> 'alex@codecourse.com',
-				'username'	=> 'alex',
-				'password'	=> bcrypt('password'),
-				'firstname'	=> 'Alex',
-				'lastname'	=> 'Garrett',
-				'location'	=> 'London, UK',
-			],
-			[
-				'email'		=> 'billy@codecourse.com',
-				'username'	=> 'billy',
-				'password'	=> bcrypt('password'),
-				'firstname'	=> 'Billy',
-				'lastname'	=> 'Garrett',
-				'location'	=> null,
-			],
-			[
-				'email'		=> 'cathy@codecourse.com',
-				'username'	=> 'cathy',
-				'password'	=> bcrypt('password'),
-				'firstname'	=> 'Cathy',
-				'lastname'	=> 'Garrett',
-				'location'	=> null,
-			],
-			[
-				'email'		=> 'dale@codecourse.com',
-				'username'	=> 'dale',
-				'password'	=> bcrypt('password'),
-				'firstname'	=> 'Dale',
-				'lastname'	=> 'Garrett',
-				'location'	=> null,
-			],
-			[
-				'email'		=> 'elma@codecourse.com',
-				'username'	=> 'elma',
-				'password'	=> bcrypt('password'),
-				'firstname'	=> 'Elma',
-				'lastname'	=> 'Garrett',
-				'location'	=> null,
-			],
-		]); // insert
+				'firstname'	=> ucfirst($name),
+				'lastname'	=> $faker->lastName(),
+			]);
+		}
 	} // run
-} // class
+}
