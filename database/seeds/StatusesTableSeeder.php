0229451df38229c01155cce7550ec995519fdf4c
Ranie Santos <ranietots32@gmail.com>
2017-06-01 04:50:01 +0800

    added boolean for toggling status reply seeding, fixed home link in 404 page

--- database/seeds/StatusesTableSeeder.php
+++ database/seeds/StatusesTableSeeder.php
@@ -10,28 +10,34 @@ class StatusesTableSeeder extends Seeder
 	{
 		Status::truncate();
 
+		$seedStatusReplies = true;
+
 		$faker = Faker\Factory::create();
 
-		foreach (range(1, 4) as $key) {
-			$timePosted = Carbon::now()->subDays(4 - $key);
+		foreach (range(20, 1) as $key) {
+			$timePosted = Carbon::now()->subDays($key);
 
 			Status::create([
-				'user_id' => $key,
+				'user_id' => ($key <= 4 ? $key : rand(1, 3)),
 				'body' => $faker->sentence(5, false),
 				'created_at' => $timePosted,
 				'updated_at' => $timePosted,
 			]);
 		}
 
-		foreach (range(1, 4) as $key) {
-			$timePosted = Carbon::now()->subHours(4 - $key);
+		if ($seedStatusReplies) {
+			$status = Status::latest()->first();
 
-			Status::find(3)->replies()->create([
-				'body' => $faker->sentence(5, false),
-				'user_id' => $key,
-				'created_at' => $timePosted,
-				'updated_at' => $timePosted,
-			]);
+			foreach (range(1, 4) as $key) {
+				$timePosted = Carbon::now()->subHours(4 - $key);
+
+				$status->replies()->create([
+					'body' => $faker->sentence(5, false),
+					'user_id' => $key,
+					'created_at' => $timePosted,
+					'updated_at' => $timePosted,
+				]);
+			}
 		}
 	} // run
 }
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,7 +5,7 @@
 		<div class="jumbotron text-center">
 			<h1>404 Error</h1>
 			<p class="text-danger">The resource you were trying to access could not be found.</p>
-			<a href="#" class="btn btn-primary btn-lg">Go to home page</a>
+			<a href="{{ route('home') }}" class="btn btn-primary btn-lg">Go to home page</a>
 		</div>
 	</div>
 @stop
